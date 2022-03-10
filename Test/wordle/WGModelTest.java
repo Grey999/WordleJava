593411d952481cbd048f24b1802084c7cb633b90
Coline Berrue <coline.berrue@epita.fr>
2022-03-10 14:24:13 +0000

    Gui: voir comment mieux disposer boutons dans input Phrase qui ne change pas pour displayerror() voir pour le placement de la grille et du keyboard Ajouter les autres flags Verifier les scanners (sc.close()) CLI: fonctionne !!!!!

---
+++ Test/wordle/WGModelTest.java
@@ -0,0 +1,16 @@
+package wordle;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WGModelTest {
+
+    @Test
+    public void initialise() {
+    }
+
+    @Test
+    public void change() {
+    }
+}
