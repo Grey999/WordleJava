0f7e9dcc358869ba2b35cf778b3ce227a60c1f3e
Coline Berrue <coline.berrue@epita.fr>
2022-04-05 23:11:02 +0100

    GUI: rajouter fonction UML Model + Notation des variables Reverifier specifications

--- Test/wordle/WGModelTest.java
+++ Test/wordle/WGModelTest.java
@@ -1,68 +1,91 @@
 package wordle;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class WGModelTest {
+    WGModel model;
+    ArrayList<String> list;
 
-    @Test
+    @Before
     public void initialise()
     {
-        WGModel model = new WGModel();
+        model = new WGModel();
         model.initialise();
+        list = new ArrayList<>();
+        list.add("robin");
+        list.add("horse");
+        list.add("array");
+        list.add("tyler");
+        list.add("blush");
+    }
+
+    @Test
+    public void FirstTest () throws FileNotFoundException {
         assertEquals(model.getPlayerword(),"");
         assertTrue(model.isFirstflag());
         assertEquals(model.getGuess(),0);
         assertEquals(model.getLastword(), "");
-    }
-
-    @Test
-    public void setRandomWord()
-    {
-        WGModel model = new WGModel();
-        model.initialise();
         model.setRandomflag(true);
-        try {
-            model.setWordtoGuess();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        assertNotEquals(model.getWordtoguess(), "absit");
-        String lastry = model.getWordtoguess();
-        try {
-            model.setWordtoGuess();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        assertNotEquals(model.getWordtoguess(), lastry);
+
+         model.setWordtoGuess();
+
+         assertNotEquals(model.getWordtoguess(), "absit");
     }
 
     @Test
-    public void setFixedWord()
-    {
-        WGModel model = new WGModel();
-        model.initialise();
+    public void SecondTest() throws FileNotFoundException {
         model.setRandomflag(false);
-        try {
-            model.setWordtoGuess();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        model.setWordtoGuess();
+        list.add("evade");
+        model.setPlayerword(list.get(0));
+        int[] oldcolors = model.getColors();
+        int oldguess = model.getGuess();
+
+        model.change();
+
+        assertNotEquals(oldcolors, model.getColors());
+        assertFalse(model.isWin());
+        assertNotEquals(oldguess, model.getGuess());
+        for(int i = model.getGuess(); i < WGModel.GUESS; i++)
+        {
+            model.setPlayerword(list.get(i));
+            model.change();
         }
-        assertEquals(model.getWordtoguess(), "absit");
-    }
+        assertFalse(model.isWin());
+        assertTrue(model.isNewgame());
 
-    @Test
-    public void changeTrue()
-    {
+
 
     }
 
     @Test
-    public void changeFalse()
-    {
+    public void ThirdTest() throws FileNotFoundException {
+        model.setRandomflag(false);
+        model.setWordtoGuess();
+        list.add(4, "absit");
+        model.setPlayerword(list.get(0));
+        int[] oldcolors = model.getColors();
+        int oldguess = model.getGuess();
 
+        model.change();
+
+        assertNotEquals(oldcolors, model.getColors());
+        assertFalse(model.isWin());
+        assertNotEquals(oldguess, model.getGuess());
+        for(int i = model.getGuess(); i < WGModel.GUESS-1; i++)
+        {
+            model.setPlayerword(list.get(i));
+            model.change();
+        }
+        model.setPlayerword(list.get(5));
+        model.change();
+        assertTrue(model.isWin());
+        assertTrue(model.isNewgame());
     }
 }
